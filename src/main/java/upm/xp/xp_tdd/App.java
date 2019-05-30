62c45b8808bfd23ef2d7162e6ba8dd08b3bed205
Alberto Alvarez Garcia <aag_uk@yahoo.com>
2019-05-30 13:21:20 -0400

    first version

---
+++ src/main/java/upm/xp/xp_tdd/App.java
@@ -0,0 +1,13 @@
+package upm.xp.xp_tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
