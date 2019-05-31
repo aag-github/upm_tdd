cf61acb20acb1aaf88e1ed234f54afe6358f6966
Alberto Alvarez Garcia <aag_uk@yahoo.com>
2019-05-31 12:00:11 -0400

    cleanup unused classes and attributes

--- src/main/java/upm/xp/xp_tdd/ClosedInterval.java
+++
@@ -1,8 +0,0 @@
-package upm.xp.xp_tdd;
-
-public class ClosedInterval extends Interval {
-
-	public ClosedInterval(double min, double max) {
-	    super(new MinClosedLimit(min), new MaxClosedLimit(max));
-	}
-}
--- src/main/java/upm/xp/xp_tdd/Interval.java
+++ src/main/java/upm/xp/xp_tdd/Interval.java
@@ -2,16 +2,12 @@ package upm.xp.xp_tdd;
 
 public class Interval {
 
-    protected double min;
-    protected double max;
     MinLimit minLimit;
     MaxLimit maxLimit;
 
     public Interval(MinLimit minLimit, MaxLimit maxLimit) {
         this.minLimit = minLimit;
         this.maxLimit = maxLimit;
-        this.min = minLimit.getValue();
-        this.max = maxLimit.getValue();
     }
     
     protected boolean isIncluded(double value) {
