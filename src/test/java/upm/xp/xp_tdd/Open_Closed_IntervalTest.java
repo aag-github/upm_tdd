6f7657ba5d9724d81370fe3775ebb0e3414517cb
Alberto Alvarez Garcia <aag_uk@yahoo.com>
2019-05-31 12:23:54 -0400

    Added Open_Closed tests

---
+++ src/test/java/upm/xp/xp_tdd/Open_Closed_IntervalTest.java
@@ -0,0 +1,41 @@
+package upm.xp.xp_tdd;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class Open_Closed_IntervalTest extends IntersectionBaseTest {
+
+    @Parameters(name = "{index}: Open_Closed: {0} {1}{2},{3}{4} {5}{6},{7}{8} returns {9}") 
+    public static Collection<Object[]> data() {
+        Object[][] values = {               
+                { "Non-overlapping_ZeroLength_Left",    "(", 4, 6, ")", "[", 0, 0, "]", false },
+                { "Non-overlapping_ZeroLength_Right",   "(", 4, 6, ")", "[", 8, 8, "]", false },
+                { "Non-overlapping_Left",               "(", 4, 6, ")", "[", 0, 2, "]", false },
+                { "Non-overlapping_Right",              "(", 4, 6, ")", "[", 8, 9, "]", false },
+                { "Adjacent_Left",                      "(", 4, 6, ")", "[", 0, 4, "]", false },
+                { "Adjacent_Right",                     "(", 4, 6, ")", "[", 6, 9, "]", false },
+                { "From_OutLeft_To_Middle",             "(", 4, 6, ")", "[", 0, 5, "]", true },
+                { "From_OutRight_To_Middle",            "(", 4, 6, ")", "[", 5, 9, "]", true },
+                { "From_OutLeft_To_RightLimit",         "(", 4, 6, ")", "[", 0, 6, "]", true },
+                { "From_OutRight_To_LeftLimit",         "(", 4, 6, ")", "[", 4, 9, "]", true },
+                { "From_OutLeft_To_OutRight",           "(", 4, 6, ")", "[", 0, 9, "]", true },
+                { "ZeroLength_On_RightLimit",           "(", 4, 6, ")", "[", 4, 4, "]", false },
+                { "ZeroLength_On_LeftLimit",            "(", 4, 6, ")", "[", 6, 6, "]", false },
+                { "From_LeftLimit_To_Middle",           "(", 4, 6, ")", "[", 4, 5, "]", true },
+                { "From_RightLimit_To_Middle",          "(", 4, 6, ")", "[", 5, 6, "]", true },
+                { "SameLimits",                         "(", 4, 6, ")", "[", 4, 6, "]", true },                
+                { "ZeroLength_InTheMiddle",             "(", 4, 6, ")", "[", 5, 5, "]", true },                
+        
+        };
+        assert(true);
+        return Arrays.asList(values);
+    }
+
+
+ 
+}
